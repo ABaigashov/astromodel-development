33e170a743cd44a81ec95ffed498a2604a67a81e
leklid <72396293+leklid@users.noreply.github.com>
2021-07-08 13:36:10 +0200

    close

--- modeling_module/physical_problems/particle_simulator/main.py
+++ modeling_module/physical_problems/particle_simulator/main.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join('..', '..')))
 sys.path.append(os.path.abspath('modeling_module'))
 
 from configurator import Configurator
-configuration = Configurator(sys.argv[1], sys.argv[2])
+configuration = Configurator._decompress(sys.argv[1], sys.argv[2])
 
 astro_object = GlobalInteraction(configuration)
 model = _model_from_config(astro_object, configuration)
