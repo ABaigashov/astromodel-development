277b96e1e7dc692bfd35baaedaaf89b23616546a
leklid <72396293+leklid@users.noreply.github.com>
2021-07-12 01:10:27 +0200

    minor fix

--- scripts/file-linker.sh
+++ scripts/file-linker.sh
@@ -8,15 +8,19 @@ setups=$PWD/modeling_module/setups
 link(){
 	if [ -f $problems/$1/config.json ]; then
 		cp -v $problems/$1/config.json $configs/$1.json
+		chmod 666 $configs/$1.json
 	fi
 	if [ -f $problems/$1/requirements.txt ]; then
 		cp -v $problems/$1/requirements.txt $requirements/$1.txt
+		chmod 666 $requirements/$1.txt
 	fi
 	if [ -f $problems/$1/setup.sh ]; then
 		cp -v $problems/$1/setup.sh $setups/$1.sh
+		chmod 666 $setups/$1.sh
 	fi
 	if [ ! -d $problems/$1/results ]; then
 		mkdir $problems/$1/results
+		chmod 777 $problems/$1/results
 	fi
 }
 
@@ -24,6 +28,7 @@ for check in $configs $requirements $setups; do
 	echo Creating $trashdir
 	if [ ! -d $check ]; then
 		mkdir $check
+		chmod 777 $check
 	fi
 done
 
