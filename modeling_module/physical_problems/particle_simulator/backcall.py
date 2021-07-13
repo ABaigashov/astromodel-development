82422dc8fe9b193f231240a149893272239ca291
leklid <72396293+leklid@users.noreply.github.com>
2021-07-13 20:20:56 +0200

    full comment backcall.py

--- modeling_module/physical_problems/particle_simulator/backcall.py
+++ modeling_module/physical_problems/particle_simulator/backcall.py
@@ -1,3 +1,19 @@
 
+##==================< INFORMATION >==================##
+##                                                   ##
+##     This file has special name 'backcall' and     ##
+##   shows how many backcalls did your code prints.  ##
+##   The 'backcall' is the step on a progress line   ##
+##   witch calls like'print(end="\b")'. If problem   ##
+##   doesn't need any progress line, you can IGNORE  ##
+##  this file and DON'T ADD it to problem directory  ##
+##                                                   ##
+##===================================================##
+
+# Define special function
+# Argument :configuration: is 'Configurator' class object
+# Returns :int: count of EXACT amount 'backcall' callings
 def value(configuration):
+	# In our case 'moviepy.editor.VideoClip' object
+	# calls 'render' function 'steps_number / frames_gap' times
 	return (configuration.steps_number // configuration.frames_gap)
