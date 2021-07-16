071b0cf93ad157eeaadf889c455139b01edd6302
leklid <72396293+leklid@users.noreply.github.com>
2021-07-17 00:08:47 +0200

    f

---
+++ scripts/boot-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ ./scripts/boot-local.sh != $BASH_SOURCE ]]; then
+	echo "Oh no... You should run this script from repository root: ./scripts/boot-local.sh [ARGS]"
+	exit 1
+fi
+if [ ! -d ./modeling_module/physical_problems/$1 ]; then
+	echo "Oh no... No such problem \"$1\", check the spelling"
+	exit 1
+fi
+
+
+bash ./scripts/file-linker.sh $1
+
+
+python3 -c "print('u noob')"
