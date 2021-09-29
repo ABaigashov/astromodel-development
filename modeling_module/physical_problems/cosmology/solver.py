d2557bc2cb8fe43d4be48c70cf118597639e0f71
Artyom1982 <48366175+Artyom1982@users.noreply.github.com>
2021-09-30 00:43:21 +0200

    новая ветка

---
+++ modeling_module/physical_problems/cosmology/solver.py
@@ -0,0 +1,32 @@
+import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.tri as mtri
+import sympy as sym
+
+
+# from mpl_toolkits.mplot3d import *
+# from matplotlib import cm
+# import matplotlib.animation as animation
+############################
+#####УРАВНЕНИЕ ПУАССОНА#####
+############################
+path = 'modeling_module/physical_problems/cosmology/'
+
+class Task_maker():
+
+    def __init__(self, config):
+        self.config = config
+
+        self.omega_m = self.config.omega_m
+        self.omega_r = self.config.omega_r
+        self.z_max = self.config.z_max
+        self.t_max = self.config.t_max
+        self.Hubble = self.config.H_0
+        self.dark_components = []
+
+        if self.config.dark_components:
+            for compts in self.config.dark_components:
+                if compts.equation_d:
+                    self.dark_components.append(compts.equation_d)
+                if compts.omega_d:
+                    self.omega_d.append(compts.omega_d)
