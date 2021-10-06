d6af633e783d141c8e3fb26b66677ecccedb2d1b
AstroBFU <48380696+AstroBFU@users.noreply.github.com>
2021-10-06 16:51:10 +0200

    Revert "Merge branch 'develop' into issue16_cosmology"
    This reverts commit e40a3211ade733ec46cf7dad7b9bf0bd4f353d29, reversing
    changes made to ddfadc91f2cc6da62f61ae95120fad58a3f6b586.

--- README.md
+++ README.md
@@ -121,7 +121,6 @@ These web pages can help you:<br>
 $ sudo ./scripts/boot/server.sh [args]
 # Acshelly we use this:
 $ sudo ./scripts/boot/server.sh up --build
-```
 
 <br>
 
--- modeling_module/physical_problems/cosmology/__init__.py
+++ modeling_module/physical_problems/cosmology/__init__.py
@@ -6,9 +6,7 @@
 
 # Import LOCAL python files with 'GlobalInteraction' object
 # and specifiend models representation of problem solution.
-from mesh_creator import MeshCreator
-
-
+from solver import Task_maker, Cosmology_data, Cosmology_calculus, Visualization
 # This is the main class. It has special name and
 # in your '__init__.py' file it MUST have same name
 # and same methods.
@@ -21,12 +19,32 @@ class Model:
 	#            :job: job instance (uses for 'job.process' monipulations)
 	def init(self, config, output, job):
 
+
+		self.Task = Task_maker(config)
 		# saving current problem model with incomming parameters
-		self.model = MeshCreator(config, output, job)
+		self.models = []
+		self.models_1 = []
+		self.model_SNE = Cosmology_data(self.Task.name_SNE, self.Task.row_SNE)
+		self.model_H = Cosmology_data(self.Task.name_Hubble, self.Task.row_Hubble)
+		self.model_SNE.Data_loader()
+		self.model_H.Data_loader()
+		for model in self.Task.cosmological_components:
+			self.models.append(Cosmology_calculus(config, model, self.model_SNE, self.model_H))
 
 	# run method
 	# must ALWAYS return path to rendered file
 	def run(self):
+		for model in self.models:
+			model.mu_diagram()
+			model.integration()
+			model.hubble_versus_z()
+			model.chi_square_hubble()
+			print(model.chi_square_H, model.H_opt)
+			self.models_1.append(model)
+
+		self.GRAPH = Visualization(self.models_1)
+
+		self.GRAPH.graphics(self.Task)
 
 		# render file and return path
-		return self.model.create_mesh()
+		return 1
