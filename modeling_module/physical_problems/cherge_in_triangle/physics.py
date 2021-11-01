f5cb65d38936e6f2c6f8d40ee0cbcd9ea406247c
AstroBFU <48380696+AstroBFU@users.noreply.github.com>
2021-11-01 13:53:01 +0200

    Create new physical problem (issue 14)

---
+++ modeling_module/physical_problems/cherge_in_triangle/physics.py
@@ -0,0 +1,104 @@
+
+# Just file with some constants.
+# Nothing special here.
+
+constants = {
+	"c": 299792458,
+	"G": 6.67408e-11,
+	"sigma_steff_bolc": 5.67036713e-8,
+	"k": 9e9,
+
+	"au": 1495978707e2,
+	"pc": 3.0856776e16,
+
+	"sun_mass": 1.989e30,
+	"sun_luminosity": 3.828e26,
+	"sun_radius": 6.9551e8,
+	"sun_temperature": 5778,
+	"sun_visual_magnitude": -26.8,
+	"sun_illumination_neare_earth": 1367,
+
+	"merc_mass": 3.3011e23,
+	"merc_radius": 2439700,
+	"merc_axis": 57909050000,
+	"merc_period": 87.969,
+	"merc_exentricity": 0.20563593,
+	"merc_inclanation": 7,
+
+	"ven_mass": 4.8675e24,
+	"ven_radius": 6051800,
+	"ven_axis": 108208930000,
+	"ven_period": 224.698,
+	"ven_exentricity": 0.0068,
+	"ven_inclanation": 3.39458,
+
+	"earth_mass": 5.97237e24,
+	"earth_radius": 6371000 ,
+	"earth_axis": 1495978707e2,
+	"earth_period": 365.256363004,
+	"earth_trop_year": 365.2421896698,
+	"earth_days": 23.934472222222222,
+	"earth_exentricity": 0.01671123,
+
+	"moon_mass": 7.3477e22,
+	"moon_radius": 1737100,
+	"moon_axis": 384399000,
+	"moon_period": 27.321661,
+	"moon_exentricity": 0.0549,
+	"moon_v_magnitude": -12.74,
+
+	"mars_mass": 6.4171e23,
+	"mars_radius": 3389500,
+	"mars_axis": 2.2794382e11,
+	"mars_period": 686.98,
+	"mars_exentricity": 0.0933941,
+	"mars_inclanation": 1.85061,
+
+	"ceres_mass": 9.393e20,
+	"ceres_radius": 473000,
+
+	"jupiter_mass": 1.89e27,
+	"jupiter_radius": 69911000,
+	"jupiter_axis": 7.785472e11,
+	"jupiter_period": 4332.589,
+	"jupiter_exentricity": 0.048775,
+	"jupiter_inclanation": 1.304,
+
+	"io_axis": 421700000,
+	"euri_nclanation": 671034000,
+	"gan_inclanation": 1070412000,
+	"kal_inclanation": 1882709000,
+
+	"saturn_mass": 5.6834e26,
+	"saturn_radius": 58232000,
+	"saturn_axis": 1426666414094.1,
+	"saturn_period": 10759.22,
+	"saturn_exentricity": 0.055723219,
+	"saturn_inclanation_sun_eq": 2.485240,
+
+	"uran_mass": 8.6810e25,
+	"uran_radius": 25362000,
+	"uran_axis": 2876679082000,
+	"uran_period": 30685.4,
+	"uran_exentricity": 0.044405586,
+	"uran_inclanation": 0.772556,
+
+	"neptun_mass": 1.02413e26,
+	"neptun_radius": 24622000,
+	"neptun_axis": 4503443661000,
+	"neptun_period": 60190.03,
+	"neptun_exentricity": 0.011214269,
+	"neptun_inclanation": 1.767975,
+
+	"plutor_mass": 1.303e22,
+	"plutor_radius": 1187000,
+	"plutor_axis": 5.9064406e12,
+	"plutor_period": 90553.02,
+	"plutor_exentricity": 0.2488273,
+	"plutor_inclanation": 17.14,
+
+	"eris_mass": 1.66e22,
+	"eris_radius": 1163000,
+	"eris_priod": 203830,
+	"eris_exentricity": 0.44068,
+}
