3093e2d0dbd3b5c158810665488fa96afeba6fde
AstroBFU <astrobfu@gmail.com>
2022-05-06 15:54:48 +0200

    Bug fix in the leopart problem setup.sh

--- modeling_module/physical_problems/galaxies_models/setup.sh
+++ modeling_module/physical_problems/galaxies_models/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+apt-get update
 apt install curl git -y
 
 curl https://cmake.org/files/v3.19/cmake-3.19.3-Linux-x86_64.sh -o cmake.sh
 bash cmake.sh --exclude-subdir --skip-license --prefix=/usr/local
 
-apt-get update
 apt-get install --no-install-recommends fenics -y
 
 git clone https://bitbucket.org/jakob_maljaars/leopart/
